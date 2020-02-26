d9b77120485c1ab25737788590229e3ce15257fc
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-26 13:07:38 +0300

    repository implemented

--- src/main/java/com/mahdidroid/spring5recipeapp/Repositories/CategoryRepository.java
+++ src/main/java/com/mahdidroid/spring5recipeapp/Repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.mahdidroid.spring5recipeapp.Repositories;
 import com.mahdidroid.spring5recipeapp.Domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/mahdidroid/spring5recipeapp/Repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/mahdidroid/spring5recipeapp/Repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,11 @@ package com.mahdidroid.spring5recipeapp.Repositories;
 import com.mahdidroid.spring5recipeapp.Domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+
+
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
