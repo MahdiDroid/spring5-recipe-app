9e77f6ec932761158f07368f060b99686da2d9fe
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-26 12:16:35 +0300

    difficulty repository difficulty

--- src/main/java/com/mahdidroid/spring5recipeapp/Repositories/DifficultyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mahdidroid.spring5recipeapp.Repositories;
-
-import com.mahdidroid.spring5recipeapp.Domain.Difficulty;
-import com.mahdidroid.spring5recipeapp.Domain.Recipe;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DifficultyRepository extends CrudRepository<Difficulty,Long> {
-}
