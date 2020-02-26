a795fe14f439f598fc4e0e38c9fb09b45b861dc4
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-26 09:46:19 +0300

    Unit of measure repository created and added

---
+++ src/main/java/com/mahdidroid/spring5recipeapp/Repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.mahdidroid.spring5recipeapp.Repositories;
+
+import com.mahdidroid.spring5recipeapp.Domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
